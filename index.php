c3f1a8b1e78b138f2582be98666c8d1a62b6727b
apwhite2013@hotmail.com <AgPiWthub94+>
2018-05-05 00:35:30 -0400

    Set up bootstrap created folders created a php header and footer for html page reuse mentally prepping to use reactjs for the first time #RTFM

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
+<?php require "./req/html_header.php"?>
+<h1>Splittish</h1>
+<div id="wrapper">
+  <button class="btn-danger">help</button>
+</div>
+<?php require "./req/html_footer.php"?>
---
+++ req/html_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="./css/my_styles.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
